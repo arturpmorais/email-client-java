9dc6e10b7da4a1bb7ca44bb32f32867dcc33045b
FelipeCorerato <fcorerato@gmail.com>
2019-06-09 16:45:53 -0300

    Adicionado o script de criptografia
    :wolf:

---
+++ Criptografia/Crypto.java
@@ -0,0 +1,65 @@
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import javax.crypto.Cipher;
+
+public class Crypto {
+    /*
+    * Estes atributos precisam seguir a seguinte formatacao:
+    *   IV                   => 16 bytes
+    *   Chave de encriptacao => 15 bytes
+    * */
+    protected String IV;
+    protected String texto;
+    protected String chaveEncriptacao;
+
+    public Crypto() {}
+
+    public Crypto(String iv, String texto, String chave) throws Exception{
+        this.setIV(iv);
+        this.setTexto(texto);
+        this.setChave(chave);
+    }
+
+    public void setIV(String iv) throws Exception{
+        if (iv == null)
+            throw new Exception ("IV ausente!");
+        if (iv.length() != 16)
+            throw new Exception ("IV deve ter 16 digitos!");
+
+        this.IV = iv;
+    }
+
+    public void setTexto(String texto) throws Exception{
+        if (texto == null)
+            throw new Exception ("Texto ausente!");
+
+        this.texto = texto;
+    }
+
+    public void setChave(String chave) throws Exception{
+        if (chave == null)
+            throw new Exception ("Chave ausente!");
+        if (chave.length() != 16)
+            throw new Exception ("A chave deve ter 15 digitos!");
+
+        this.chaveEncriptacao = chave;
+    }
+
+    public byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+        Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+        SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+        encripta.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(this.IV.getBytes("UTF-8")));
+
+        return encripta.doFinal(textopuro.getBytes("UTF-8"));
+    }
+
+    public String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception{
+        Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+        SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+        decripta.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(this.IV.getBytes("UTF-8")));
+
+        return new String(decripta.doFinal(textoencriptado),"UTF-8");
+    }
+
+}
---
+++ Criptografia/Teste.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Teste {
+    static String IV = "AAAAAAAAAAAAAAAA";
+    static String chaveencriptacao = "0123456789abcdef";
+
+    public static void main(String [] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String texto = br.readLine();
+
+            Crypto crypto = new Crypto(IV, texto, chaveencriptacao);
+
+            System.out.println("Texto Puro: " + texto);
+
+            byte[] textoencriptado = crypto.encrypt(texto, chaveencriptacao);
+
+            System.out.print("Texto Encriptado: ");
+
+            for (int i = 0; i < textoencriptado.length; i++)
+                System.out.print(new Integer(textoencriptado[i])+" ");
+
+            System.out.println("\n");
+
+            String textodecriptado = crypto.decrypt(textoencriptado, chaveencriptacao);
+
+            System.out.println("Texto Decriptado: " + textodecriptado);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
